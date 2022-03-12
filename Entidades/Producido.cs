ae08b3b98356ae82eedcf7007e713ee69b0dd420
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-12 17:06:21 -0400

    Create Producido.cs

---
+++ Entidades/Producido.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JhonAlbertGuzman_P2.Entidades
+{
+    public class Producido
+    {
+        public int Id { get; set; }
+        public int ProductoEmpaqueId { get; set; }
+        public List<string> ListaProductos { get; set;}
+        public int cantidad { get; set; }
+    }
+}
