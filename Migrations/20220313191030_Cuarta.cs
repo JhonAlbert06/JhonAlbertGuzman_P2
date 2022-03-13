3be9da2a42626e64e9e7f7b86cd7cd014a22387f
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-13 17:21:02 -0400

    Migracion Cuarta

---
+++ Migrations/20220313191030_Cuarta.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JhonAlbertGuzman_P2.Migrations
+{
+    public partial class Cuarta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Producido_ProductosEmpaque_ProductoId",
+                table: "Producido");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Producido_ProductoId",
+                table: "Producido");
+
+            migrationBuilder.DropColumn(
+                name: "ProductoId",
+                table: "Producido");
+
+            migrationBuilder.RenameColumn(
+                name: "cantidad",
+                table: "Producido",
+                newName: "Cantidad");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Producido",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producido_ProductoEmpaqueId",
+                table: "Producido",
+                column: "ProductoEmpaqueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Producido_ProductosEmpaque_ProductoEmpaqueId",
+                table: "Producido",
+                column: "ProductoEmpaqueId",
+                principalTable: "ProductosEmpaque",
+                principalColumn: "ProductoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Producido_ProductosEmpaque_ProductoEmpaqueId",
+                table: "Producido");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Producido_ProductoEmpaqueId",
+                table: "Producido");
+
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Producido");
+
+            migrationBuilder.RenameColumn(
+                name: "Cantidad",
+                table: "Producido",
+                newName: "cantidad");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductoId",
+                table: "Producido",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producido_ProductoId",
+                table: "Producido",
+                column: "ProductoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Producido_ProductosEmpaque_ProductoId",
+                table: "Producido",
+                column: "ProductoId",
+                principalTable: "ProductosEmpaque",
+                principalColumn: "ProductoId");
+        }
+    }
+}
