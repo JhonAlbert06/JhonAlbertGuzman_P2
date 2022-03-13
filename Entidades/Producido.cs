9eddb8a8953cc0736fc2d096bbe297939d52ae6d
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-13 17:20:41 -0400

    Update Producido.cs

--- Entidades/Producido.cs
+++ Entidades/Producido.cs
@@ -5,8 +5,24 @@ namespace JhonAlbertGuzman_P2.Entidades
 {
     public class Producido
     {
+        [Key]
         public int Id { get; set; }
         public int ProductoEmpaqueId { get; set; }
-        public int cantidad { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+
+        public Producido()
+        {
+            Id = 0;
+            ProductoEmpaqueId = 0;
+            Cantidad = 0;
+            Descripcion = null;
+        }
+
+        public Producido(int cantidad, string descripcion)
+        {
+            Cantidad = cantidad;
+            Descripcion = descripcion;
+        }
     }
 }
