26fd39c698b728ba25ea24a4076f22a0d79afc49
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-14 12:02:35 -0400

    Update ProductosEmpaque.cs

--- Entidades/ProductosEmpaque.cs
+++ Entidades/ProductosEmpaque.cs
@@ -13,10 +13,6 @@ namespace JhonAlbertGuzman_P2.Entidades
         [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string Concepto { get; set; }
 
-        [Required(ErrorMessage = "La cantidad no puede estar vacia...")]
-        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe estar en el rango de {1} y {2}.")]
-        public int CantidadUtilizados { get; set; }
-
         [ForeignKey("ProductoId")]
         public List<Utilizados> Utilizados { get; set; } = new List<Utilizados>();
         
