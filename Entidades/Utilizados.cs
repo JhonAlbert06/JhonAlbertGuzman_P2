5b71e98da7416feb7f567f99636637d70038bcf1
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-14 09:57:55 -0400

    Create Utilizados.cs

---
+++ Entidades/Utilizados.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JhonAlbertGuzman_P2.Entidades
+{
+    public class Utilizados
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductoEmpaqueId { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+
+        public Utilizados()
+        {
+            Cantidad = 0;
+            Descripcion = null;
+        }
+
+        public Utilizados(int cantidad, string descripcion)
+        {
+            Cantidad = cantidad;
+            Descripcion = descripcion;
+        }
+    }
+}
