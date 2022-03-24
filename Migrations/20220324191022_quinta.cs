74deeba25c3a405e1bc910f6dbad9f94fec247a4
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-24 16:08:31 -0400

    Migracion quinta

---
+++ Migrations/20220324191022_quinta.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JhonAlbertGuzman_P2.Migrations
+{
+    public partial class quinta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Cantidad",
+                table: "ProductosEmpaque",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cantidad",
+                table: "ProductosEmpaque");
+        }
+    }
+}
