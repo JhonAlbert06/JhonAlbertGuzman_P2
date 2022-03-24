6758aa74dc0dbb14f5251ab7b8947033a46af3d6
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-24 18:09:07 -0400

    septima migracion

---
+++ Migrations/20220324215252_septima.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JhonAlbertGuzman_P2.Migrations
+{
+    public partial class septima : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
