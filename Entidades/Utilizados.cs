5ceeee34db722b130feb2be7ca5cedb3b4cf6c5c
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-25 20:29:55 -0400

    Update Utilizados.cs

--- Entidades/Utilizados.cs
+++ Entidades/Utilizados.cs
@@ -7,7 +7,6 @@ namespace JhonAlbertGuzman_P2.Entidades
     {
         [Key]
         public int Id { get; set; }
-        public int ProductoId { get; set; }
         public int Cantidad { get; set; }
         public string Descripcion { get; set; }
         
