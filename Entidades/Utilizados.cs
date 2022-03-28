b1883e60346ead4f4baefa50b637df7070c48de4
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-28 16:20:32 -0400

    Update Utilizados.cs

--- Entidades/Utilizados.cs
+++ Entidades/Utilizados.cs
@@ -8,7 +8,12 @@ namespace JhonAlbertGuzman_P2.Entidades
         [Key]
         public int Id { get; set; }
         public int UtilizadoId { get; set; }
+        
+        [Required(ErrorMessage = "La Cantidad no puede estar vacia...")]
+        [Range(1, int.MaxValue, ErrorMessage = "La Cantidad debe estar en el rango de {1} y {2}.")]
         public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string Descripcion { get; set; }
         
         [ForeignKey("ProductoId")] 
