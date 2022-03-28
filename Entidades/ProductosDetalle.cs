a8643b501b97576f19bda008f5570b7a13b3d8d7
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-03-28 16:20:25 -0400

    Update ProductosDetalle.cs

--- Entidades/ProductosDetalle.cs
+++ Entidades/ProductosDetalle.cs
@@ -8,7 +8,12 @@ namespace JhonAlbertGuzman_P2.Entidades
         [Key]
         public int Id { get; set; }
         public int ProductoId { get; set; }
+        
+        [Required(ErrorMessage = "Es obligatorio introducir la descripcion")]
         public string Presentacion { get; set; }
+
+        [Required(ErrorMessage = "La Cantidad no puede estar vacia...")]
+        [Range(1, int.MaxValue, ErrorMessage = "La Cantidad debe estar en el rango de {1} y {2}.")]
         public double Cantidad { get; set; }
         public double Precio { get; set; }
         public double Costo { get; set; }
